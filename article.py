9d79ade2e2ae08630ed7b35111d50cf685127e21
Brune <brune.alirand@edu.devinci.fr>
2026-09-10 15:05:00 +0200

    Add article functions: create/read/delete

--- article.py
+++ article.py
@@ -1,5 +1,5 @@
 
-#it returns the name of the article if its exists 
+#contains fonctions to read, create and handle article's files
 
 from pathlib import Path
 
@@ -19,6 +19,8 @@ def get_articles() -> list[Path]:
     return list(ARTICLES_DIR.glob("*.md"))
 
 
+#returns the article's path 
+
 def read_article(article_url: str) -> str | None:
     article_path = get_article_path(article_url)
 
@@ -27,27 +29,9 @@ def read_article(article_url: str) -> str | None:
 
     return article_path.read_text(encoding="utf-8")
 
+#creates an article 
+
 
 def create_article(article_url: str, content: str) -> None:
     article_path = ARTICLES_DIR / f"{article_url}.md"
     article_path.write_text(content, encoding="utf-8")
-
-#supprimer un article 
-
-@app.get("/article/{articleurl}/delete")
-def delete_article(articleurl: str):
-    source = ARTICLES_DIR / f"{articleurl}.md"
-    destination = ARTICLES_DIR.parent / "trash" / source.name
-
-    if not source.exists():
-        raise HTTPException (
-            status_code=404,
-            detail="The article does not exist."
-        )
-
-    if destination.exists():
-        destination.unlink()
-
-    source.rename(destination)
-
-    return {"deleted": True}
